1bba2958e2674ff6dbb1132df2137613b7a66731
JM Olesco <jmolesco@yahoo.com>
2019-04-18 23:43:41 -0700

    changes
    new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
