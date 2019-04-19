3ba85c5e219b580ecf85c6f8ebb593f1c88fd576
JM Olesco <jmolesco@yahoo.com>
2019-04-18 23:38:42 -0700

    Updates

---
+++ precache-manifest.9982b2eaa3f46fde29e0e6fb1472d9ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "234bcb34a400a42c546a5b1c96e9fc5e",
+    "url": "/carecenter/static/media/logos.234bcb34.png"
+  },
+  {
+    "revision": "6b4146056a87ed745886486d6b87ecab",
+    "url": "/carecenter/static/media/doc.6b414605.jpg"
+  },
+  {
+    "revision": "a7ec9448e79cc1bca479",
+    "url": "/carecenter/static/js/runtime~main.0896087a.js"
+  },
+  {
+    "revision": "dd2ea37826779acba65b",
+    "url": "/carecenter/static/js/main.6fe7adeb.chunk.js"
+  },
+  {
+    "revision": "1e1918e87fbd46f3f92e",
+    "url": "/carecenter/static/js/2.2970d2b1.chunk.js"
+  },
+  {
+    "revision": "dd2ea37826779acba65b",
+    "url": "/carecenter/static/css/main.51276c5e.chunk.css"
+  },
+  {
+    "revision": "47c1030a69ae332e5184188b614dc76b",
+    "url": "/carecenter/index.html"
+  }
+];
