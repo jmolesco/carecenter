3a0ff44d10a316138fb77eeb2286d45b5a3d2cc0
JOHN MARK OLESCO <johnmark@ivp.co.jp>
2019-04-26 16:03:26 +0800

    new-update-insurance

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -7,20 +7,7 @@ class AboutUs extends Component {
     {
         return (
             <Container>
-                <Row className="justify-content-md-center">
-                    <Col md={12}>
-                        <Card className="text-left">
-                            <Card.Header><b>&raquo; Our Doctors Staff</b></Card.Header>
-                            <Card.Body>
-                                <Card.Text>
-                                    The Care Center for Allergy and Asthma’s doctor and staff have been providing high quality and comprehensive care for conditions relating to Allergy, Asthma, and Immunology in the Northwest suburbs of Chicago for the past 25 years. The staff is led by a board certified allergist, asthma specialist, and immunologist who believes in a holistic approach to treatment. Our physician offers state of the art allergy and asthma evaluation. She utilizes an array of treatment options suited to the needs and conditions of her individual patients. Among many of her therapeutic options are allergy contact avoidance which may include environmental control and food avoidance, nutritional counseling, dietary management, medications, and allergy immunotherapy. Our physician strives for accurate and efficient diagnosis as well as immediate and long lasting allergy relief for patients.                
-                                    Our staff consists of well trained medical and office personnel who are courteous and eager to assist with patient needs. We provide quality and individual care.The billing department bills most insurance companies and assists patients with their claims. We also offer a variety of payment plans and options suited to the patients’ needs and budget.
-                                </Card.Text>
-                            </Card.Body>
-                            </Card>
-                    </Col>
-                </Row>
-                <Row className="justify-content-md-left">
+                 <Row className="justify-content-md-left">
                     <Col md={4}>
                         <Card className="text-left">
                             <center>
@@ -33,7 +20,7 @@ class AboutUs extends Component {
                     </Col>
                     <Col md={8}>
                         <Card className="text-center">
-                            <Card.Header>About her</Card.Header>
+                            <Card.Header>About the Doctor</Card.Header>
                                 <Card.Body>
                                     <Card.Text>
                                     Dr. Cynthia Yango-Eugenio is certified by the American Board of Allergy, Asthma and Immunology.   She is a solo Allergy, Asthma and Immunology practitioner for the past 30 years in Buffalo Grove, Illinois.She has been an active member of the Chicago-Northwestern Indiana chapter of the
--- src/components/Insurance/Insurance.js
+++ src/components/Insurance/Insurance.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, CardDeck, Button, Image,  Carousel, Alert,Card,ListGroup,CardColumns } from 'react-bootstrap';
+import cigna from  '../../images/cigna.PNG';
+import coresource from  '../../images/coresource.png';
 
 class Insurance extends Component {
 
@@ -33,10 +35,10 @@ insurance plans. </p>
                             </Card>
                             <Card>
                                 <center>
-                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.aetna.com/content/dam/aetna/images/logos/1_Heart_Aetna_logo_sm_rgb_violet.png"} />
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://image4.owler.com/logo/allied-insurance_owler_20180906_003049_original.jpg"} />
                                 </center>
                                 <Card.Footer className="text-center">
-                                    <small className="text-muted text-center">Aetna Better Health</small>
+                                    <small className="text-muted text-center">Allied</small>
                                 </Card.Footer>                                
                             </Card>
                             <Card>
@@ -44,7 +46,7 @@ insurance plans. </p>
                                     <Card.Img variant="top" className="card-img-insurance" src={"https://www.mysmarthealth.org/resources/images/Ascension_SmartHealth_graphic_fc_rgb_300.png"} />
                                 </center>
                                 <Card.Footer className="text-center">
-                                    <small className="text-muted text-center">Ascension Smarthealth Assurant</small>
+                                    <small className="text-muted text-center">Ascension Smarthealth</small>
                                 </Card.Footer>
                             </Card>
                         </CardDeck>
@@ -54,14 +56,37 @@ insurance plans. </p>
                 <Row className="justify-content-md-center 2nd-col">
                    <Col md={12}>
                    <CardDeck>
+                             <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.assurant.com/images/assurant/nextgen/assurant-logo.png?sfvrsn=2ad9be04_2"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Assurant</small>
+                                </Card.Footer>
+                            </Card>
                             <Card>
                                 <center>
                                     <Card.Img variant="top" className="card-img-insurance" src={"https://www.bcbs.com/themes/custom/bcbs/img/full-logo.svg"} />
                                 </center>
                                 <Card.Footer className="text-center">
-                                    <small className="text-muted text-center">Blue Cross Blue Shield</small>
+                                    <small className="text-muted text-center">Blue Cross Blue Shield PPO</small>
                                 </Card.Footer>
                             </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.bcbs.com/themes/custom/bcbs/img/full-logo.svg"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Blue Cross Blue Shield HMO W/ Referral</small>
+                                </Card.Footer>
+                            </Card>
+                        </CardDeck>
+             
+                   </Col>
+                </Row>
+                <Row className="justify-content-md-center 3rd-col">
+                   <Col md={12}>
+                   <CardDeck>
                             <Card>
                                 <center>
                                     <Card.Img variant="top" className="card-img-insurance" src={"https://www.bcbsil.com/bcchp/img/bcbsil-logo.png"} />
@@ -72,19 +97,154 @@ insurance plans. </p>
                             </Card>
                             <Card>
                                 <center>
-                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.geha.com/images/default-source/geha-logos/geha.png?sfvrsn=bcf8cb8b_2"} />
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.bcbsil.com/bcchp/img/bcbsil-logo.png"} />
                                 </center>
                                 <Card.Footer className="text-center">
-                                    <small className="text-muted text-center">Coventry Geha</small>
+                                    <small className="text-muted text-center">Blue Cross Medicare Advantage</small>
+                                </Card.Footer>                                
+                            </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={cigna}  />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Cigna PPO & Open Access Plans</small>
                                 </Card.Footer>
                             </Card>
                         </CardDeck>
              
                    </Col>
                 </Row>
-               
-               
-          </Container>
+                <Row className="justify-content-md-center 4th-col">
+                   <Col md={12}>
+                   <CardDeck>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={coresource} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">CoreSource</small>
+                                </Card.Footer>                                
+                            </Card>
+
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.geha.com/images/default-source/geha-logos/geha.png?sfvrsn=bcf8cb8b_2"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Coventry Geha</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://arrowheadcenter.nmsu.edu/wp-content/uploads/2018/08/HealthSprint-logo.png"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">HealthSprint</small>
+                                </Card.Footer>                                
+                            </Card>
+                        </CardDeck>
+             
+                   </Col>
+                </Row>
+                <Row className="justify-content-md-center 5th-col">
+                   <Col md={12}>
+                   <CardDeck>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.humana.com/dist/hcom/static/icons/humana-logo.svg"}  />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Humana</small>
+                                </Card.Footer>                                
+                            </Card>
+
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.illinois.gov/hfs/PublishingImages/HFSLogoCurveV2.png"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Illinois Medicaid</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Meridian</small>
+                                </Card.Footer>                                
+                            </Card>
+                        </CardDeck>
+             
+                   </Col>
+                </Row>
+                <Row className="justify-content-md-center 6th-col">
+                   <Col md={12}>
+                   <CardDeck>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.meritain.com/Media/Default/images/logo.png"}  />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Meritain Health</small>
+                                </Card.Footer>                                
+                            </Card>
+
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://static.wixstatic.com/media/b1032a_5c45308387f94e048bc42a00611b49f0~mv2.png/v1/fill/w_224,h_84,al_c,q_80,usm_0.66_1.00_0.01/logo-for-web.webp"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">PHCS(Private Healthcare Systems)</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src="https://www.humanamilitary.com/media/Sites/HumanaMilitary/logo/hum_tricare_logo_rgb_pos.png" />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Tricare East</small>
+                                </Card.Footer>                                
+                            </Card>
+                        </CardDeck>
+             
+                   </Col>
+                </Row>
+                <Row className="justify-content-md-center 7th-col">
+                   <Col md={12}>
+                   <CardDeck>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src="http://www.upnus.com/images/unified-physicians-network.jpg"  />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">Unified Physiciams Network W/ Referral</small>
+                                </Card.Footer>                                
+                            </Card>
+
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.uhc.com/content/dam/uhcdotcom/en/responsive/header-logo.png"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">United Healthcare</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+                                <center>
+                                    <Card.Img variant="top" className="card-img-insurance" src={"https://www.uhc.com/content/dam/uhcdotcom/en/responsive/header-logo.png"} />
+                                </center>
+                                <Card.Footer className="text-center">
+                                    <small className="text-muted text-center">United Healthcare Medicare Advantage</small>
+                                </Card.Footer>                                
+                            </Card>
+                        </CardDeck>
+             
+                   </Col>
+                </Row>
+
+         </Container>
         );
     }
 }
